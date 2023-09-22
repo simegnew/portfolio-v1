07493f55496da1fffcc70315021a31f6c7def204
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2023-09-22 19:52:47 +0300

    add profile image

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,5 +1,7 @@
 import './header.css'
 import data from './data'
+import profile from '../../assets/header/abiye (2).jpg'
+
 // import headerProfile from '../../assets/header/logo.png'
 
 const Header = () => {
@@ -7,10 +9,7 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <div className="header__profile">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkc4LWWF44R2wHmBu4z9ZEvp2-M0xkgMdqbw23I-XMk8gSrAZDAkWatBtHINGhFjSODN0&usqp=CAU"
-            alt="profile"
-          />
+          <img src={profile} alt="profile" />
         </div>
         <h3>Simegnew Tizazu</h3>
         <p>
@@ -19,15 +18,24 @@ const Header = () => {
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
         <div className="header__cta">
-          <a href="#contact">Let's Talk</a>
-          <a href="#portfolio">My Work</a>
+          <a href="#contact" className="btn primary">
+            Let's Talk
+          </a>
+          <a href="#portfolio" className="btn light">
+            My Work
+          </a>
         </div>
-        <div className='header__socials' >
-          {
-            data.map(item => (
-              <a key={item.id} target='_blanck' href={item.link} rel='noopener noreferrer'>{ item.icon }</a>
-            ))
-          }
+        <div className="header__socials">
+          {data.map((item) => (
+            <a
+              key={item.id}
+              target="_blanck"
+              href={item.link}
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          ))}
         </div>
       </div>
     </header>
