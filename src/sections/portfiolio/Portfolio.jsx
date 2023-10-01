96748a3b3a0d772a124cbdd4a1c98106629fb170
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2023-10-01 11:59:44 +0300

    add the portfolio category

--- src/sections/portfiolio/Portfolio.jsx
+++ src/sections/portfiolio/Portfolio.jsx
@@ -1,34 +1,70 @@
+import { useState } from 'react';
 import Card from '../../components/Card';
 import data from './data';
 import './portfolio.css'
-import portimg from "../../assets/portfolio-1.jpeg";
+// import portimg from "../../assets/portfolio-1.jpeg";
 
 
-const Portfolio = () => {
+const Portfolio = () => { 
+
+  const [isPressed, setIsPressed] = useState(null);
+
+  
+  const handleBtnPressed = (category) => {
+    setIsPressed(category);
+  }
+  
   return (
-    <section  id="portfolio">
+    <section id="portfolio">
       <h2>portfolio</h2>
       <p>
         Check Out some of the projects i recently worked on for my clients. use
         the button s to toggle the different categories.
       </p>
-      <div className="portfolio__items container">
-        {/* <div></div> */}
+      <div className=" container">
+        <div className="portfolio__items">
+          <button
+            className={`btn ${isPressed === "All" ? "active" : "light"}`}
+            onClick={() => handleBtnPressed("All")}
+          >
+            All
+          </button>
+          <button
+            className={`btn ${isPressed === "front" ? "active" : "light"}`}
+            onClick={() => handleBtnPressed("front")}
+          >
+            Frontend
+          </button>
+          <button
+            className={`btn ${isPressed === "back" ? "active" : "light"}`}
+            onClick={() => handleBtnPressed("back")}
+          >
+            Backend
+          </button>
+        </div>
 
         <div className="portfolio__items-1">
-          {data.map((item) => (
-            <Card classsName="single__portfolio" key={item.id}>
-              <div className="portfolio__img">
-                <img src={item.image} alt="myproject- images" />
-              </div>
-              <span>{item.title}</span>
-              <small>{item.desc}</small>
-              <div className="portfolio_cta">
-                <a className='btn light btn_portfolio' href={item.demo}>Demo</a>
-                <a className='btn primary' href={item.prolink}>Github</a>
-              </div>
-            </Card>
-          ))}
+          {isPressed === 'All' ?
+          (  data.map((item) => (
+              <Card classsName="single__portfolio" key={item.id}>
+                <div className="portfolio__img">
+                  <img src={item.image} alt="myproject- images" />
+                </div>
+                <span>{item.title}</span>
+                <small>{item.desc}</small>
+                <div className="portfolio_cta">
+                  <a className="btn light btn_portfolio" href={item.demo}>
+                    Demo
+                  </a>
+                  <a className="btn primary" href={item.prolink}>
+                    Github
+                  </a>
+                </div>
+              </Card>
+          ))) : (
+              <div>hello</div>
+            )
+          }
         </div>
       </div>
     </section>
