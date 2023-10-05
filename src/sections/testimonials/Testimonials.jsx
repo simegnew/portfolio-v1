a75af635de3ce0a32f163f1a27a0211f42aa3297
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2023-10-05 09:42:33 +0300

    add testimonial

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,11 +1,61 @@
-import './testimonials.css'
+import React, { useState } from "react";
+import Card from "../../components/Card";
+import data from "./data";
+import "./testimonials.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+// import "swiper/css/navigation";
+import "swiper/css/effect-fade";
+import { EffectCoverflow, Pagination } from 'swiper/modules'
 
 const Testimonials = () => {
+  
   return (
-    <section>
-        <h2>testimonials</h2>
-    </section>
-  )
-}
+    <section className="container" id="testimonials">
+      <h2>What My Clients Say</h2>
+      <p>These are unbaised testimonials from some of my clients</p>
 
-export default Testimonials
+      <Swiper
+        spaceBetween={50}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={3}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 20,
+        }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
+        modules={[EffectCoverflow, Pagination]}
+        className="swiper_container"
+      >
+        {data.map((item) => (
+          <SwiperSlide>
+            <Card classsName="testimonial_item" key={item.id}>
+              <span>{item.desc}</span>
+              <div className="testimonial_image">
+                <img src={item.image} alt="client-image" />
+                <div>
+                  <span>{item.name}</span>
+                  <small>{item.title}</small>
+                </div>
+              </div>
+            </Card>
+          </SwiperSlide>
+        ))}
+
+        <div className="slider-controler">
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
+      </section>
+  );
+};
+
+export default Testimonials;
---
+++ src/sections/testimonials/data.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    id: 1,
+    name: "abebe",
+    title: "web Developer",
+    desc: "L neque vel ultrices. Integer eleifend ligula sed rutrum dignissim.",
+    image:
+      "https://0.academia-photos.com/10416941/3640010/4268998/s200_simegnew.tizazu.jpg",
+  },
+  {
+    id: 2,
+    name: "abebe",
+    title: "web Developer",
+    desc: "Loneque vel ultrices. Integer eleifend ligula sed rutrum dignissim.",
+    image:
+      "https://0.academia-photos.com/10416941/3640010/4268998/s200_simegnew.tizazu.jpg",
+  },
+  {
+    id: 3,
+    name: "abebe",
+    title: "web Developer",
+    desc: "neque vel ultrices. Integer eleifend ligula sed rutrum dignissim.",
+    image:
+      "https://0.academia-photos.com/10416941/3640010/4268998/s200_simegnew.tizazu.jpg",
+  },
+  {
+    id: 4,
+    name: "abebe",
+    title: "web Developer",
+    desc: "L neque vel ultrices. Integer eleifend ligula sed rutrum dignissim.",
+    image:
+      "https://0.academia-photos.com/10416941/3640010/4268998/s200_simegnew.tizazu.jpg",
+  },
+  {
+    id: 5,
+    name: "abebe",
+    title: "web Developer",
+    desc: "Loneque vel ultrices. Integer eleifend ligula sed rutrum dignissim.",
+    image:
+      "https://0.academia-photos.com/10416941/3640010/4268998/s200_simegnew.tizazu.jpg",
+  },
+];
+    
+export default data;
