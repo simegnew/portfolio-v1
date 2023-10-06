548a8698a9b7e0fb6cd6fb891274718fa0423b3a
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2023-10-06 15:01:05 +0300

    add the contact section

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,11 +1,33 @@
 import './contact.css'
+import data from './data';
 
 const Contact = () => {
   return (
-    <section>
-        <h2>contact</h2>
+    <section id="contact">
+      <h2>Get In Touch</h2>
+      <p>Shoot me a message via any of the links below.</p>
+      <div className="container contact__container">
+        {data.map((item) => (
+          <a
+            className="contact__link"
+            href={item.link}
+            target="_blank"
+          >
+            {item.icon}
+          </a>
+        ))}
+        {/* <a href="#" className="btn primary contact__link">
+          messsage
+        </a>
+        <a href="#" className="btn primary contact__link">
+          messsage
+        </a>
+        <a href="#" className="btn primary contact__link">
+          messsage
+        </a> */}
+      </div>
     </section>
-  )
+  );
 }
 
 export default Contact
---
+++ src/sections/contact/data.js
@@ -0,0 +1,11 @@
+import {AiOutlineMail} from 'react-icons/ai'
+import {ImLinkedin2} from 'react-icons/im'
+import { BsTelegram } from "react-icons/bs";
+
+const data = [
+  { id: 1, link: "mailTo:abebetizazu157@gmail.com", icon: <AiOutlineMail /> },
+  { id: 2, link: "https://www.linkedin.com/in/abe-ti/", icon: <ImLinkedin2 /> },
+  { id: 3, link: "https://t.me/abiye1921", icon: <BsTelegram /> },
+];
+
+export default data;
